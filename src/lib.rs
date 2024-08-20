b3e73e9cb8602238ae670f31dcd64be2acabc9aa
Riku Takeuchi <rikuson@users.noreply.github.com>
2024-08-20 12:01:46 +0900

    feat: add initial GStreamer plugin setup
    This commit introduces the initial setup for the gst-voicevox project.
    It includes the Cargo.toml configuration, build script, and the main
    library file with a basic GStreamer plugin definition. The plugin is
    designed to provide text-to-speech functionality using VOICEVOX.

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use gst::glib;
+
+gst::plugin_define!(
+    voicevoxtts,
+    env!("CARGO_PKG_DESCRIPTION"),
+    plugin_init,
+    concat!(env!("CARGO_PKG_VERSION"), "-", env!("COMMIT_ID")),
+    "MIT/X11",
+    env!("CARGO_PKG_NAME"),
+    env!("CARGO_PKG_NAME"),
+    env!("CARGO_PKG_REPOSITORY"),
+    env!("BUILD_REL_DATE")
+);
+
+fn plugin_init(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
+    Ok(())
+}
